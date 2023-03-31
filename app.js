064355cd7366865bd330ba741343636e4aa75f8b
Treasure <mac@MACs-MacBook-Pro.local>
2023-03-31 04:31:51 +0100

    final commit🥷🏾

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
- AOS.init();
+// import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+
+// document.getElementById('parallax').animate(
+//     { transform: ['translateY(0)', 'translateY(100px)']},
+//     { duration: 10000, // Totally arbitrary!
+//       fill: 'both',
+//       timeline: new ScrollTimeline({
+//           scrollOffsets: [
+//               new CSSUnitValue(0, 'px'),
+//               new CSSUnitValue(200, 'px')
+//           ]
+//       })
+//     });
